91b5d8bfda5805f6cf6630a1e40db58f956c66c7
Nima <197469689+Nima-Mohammadkhani@users.noreply.github.com>
2025-08-04 19:39:59 +0330

    add emptyCartScreen component

--- food-ordering-app/app/(tabs)/cart/index.tsx
+++ food-ordering-app/app/(tabs)/cart/index.tsx
@@ -1,30 +1,10 @@
-import Button from "@/components/ui/Button";
+import EmptyCartScreen from "@/components/EmptyCartScreen";
 import { View, Text, SafeAreaView, StatusBar, Image } from "react-native";
-import { useRouter } from "expo-router";
 const CartScreen = () => {
-  const router = useRouter();
   return (
     <SafeAreaView className="flex-1">
       <StatusBar hidden />
-      <View className="flex-1 flex justify-center items-center gap-4">
-        <Image
-          source={require("@/assets/images/emptyCart.png")}
-          resizeMode="cover"
-          className="self-center mb-6"
-          style={{ transform: [{ scale: 1.5 }] }}
-        />
-        <Text className="text-4xl font-bold mt-">Ouch! Hungry</Text>
-        <Text className="text-base text-gray-500">
-          Seems like you have not ordered any food yet
-        </Text>
-        <Button
-          title="Find Foods"
-          size="md"
-          textClassName="text-white"
-          className="bg-[#FE8C00] rounded-full"
-          onPress={() => router.push("/")}
-        />
-      </View>
+      <EmptyCartScreen />
     </SafeAreaView>
   );
 };
---
+++ food-ordering-app/components/emptyCartScreen.tsx
@@ -0,0 +1,28 @@
+import { View, Image, Text } from "react-native";
+import Button from "./ui/Button";
+import { useRouter } from "expo-router";
+const EmptyCartScreen = () => {
+  const router = useRouter();
+  return (
+    <View className="flex-1 flex justify-center items-center gap-4">
+      <Image
+        source={require("@/assets/images/emptyCart.png")}
+        resizeMode="cover"
+        className="self-center mb-6"
+        style={{ transform: [{ scale: 1.5 }] }}
+      />
+      <Text className="text-4xl font-bold mt-">Ouch! Hungry</Text>
+      <Text className="text-base text-gray-500">
+        Seems like you have not ordered any food yet
+      </Text>
+      <Button
+        title="Find Foods"
+        size="md"
+        textClassName="text-white"
+        className="bg-[#FE8C00] rounded-full"
+        onPress={() => router.push("/")}
+      />
+    </View>
+  );
+};
+export default EmptyCartScreen;
