ba32e3d79523f0337de5e3dc0146271fbf92f6f9
Nima <197469689+Nima-Mohammadkhani@users.noreply.github.com>
2025-08-04 10:21:27 +0330

    enhance layout and add empty cart image with messaging

--- food-ordering-app/app/(tabs)/cart/index.tsx
+++ food-ordering-app/app/(tabs)/cart/index.tsx
@@ -1,13 +1,31 @@
-import { View, Text } from "react-native";
-
+import Button from "@/components/ui/Button";
+import { View, Text, SafeAreaView, StatusBar, Image } from "react-native";
+import { useRouter } from "expo-router";
 const CartScreen = () => {
+  const router = useRouter();
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-2xl font-bold">Cart</Text>
-      <Text className="text-xl mt-4 text-gray-600">
-        Your selected products will be displayed here.
-      </Text>
-    </View>
+    <SafeAreaView className="flex-1">
+      <StatusBar hidden />
+      <View className="flex-1 flex justify-center items-center gap-4">
+        <Image
+          source={require("@/assets/images/emptyCart.png")}
+          resizeMode="cover"
+          className="self-center mb-6"
+          style={{ transform: [{ scale: 1.5 }] }}
+        />
+        <Text className="text-4xl font-bold mt-">Ouch! Hungry</Text>
+        <Text className="text-base text-gray-500">
+          Seems like you have not ordered any food yet
+        </Text>
+        <Button
+          title="Find Foods"
+          size="md"
+          textClassName="text-white"
+          className="bg-[#FE8C00] rounded-full"
+          onPress={() => router.push("/")}
+        />
+      </View>
+    </SafeAreaView>
   );
 };
 export default CartScreen;
